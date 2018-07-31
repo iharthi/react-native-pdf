5162ab6c17f3370e292334da34c73bbcbd55c9e2
Sergey Kovalskiy <sergey@kovalskiy.net>
2018-07-31 19:49:37 +0300

    Change the way PDFs are rendered
    Use intermediate image to render PDF as image with arbitrary dimensions.
    
    This was done as a part of work for an app using this library.
    So far, this does not improve quality of PDFs on ios.

--- ios/RCTPdf/RCTPdfPageView.m
+++ ios/RCTPdf/RCTPdfPageView.m
@@ -78,7 +78,9 @@
     CGContextRef context = UIGraphicsGetCurrentContext();
     
     // PDF page drawing expects a Lower-Left coordinate system, so we flip the coordinate system before drawing.
-    CGContextScaleCTM(context, 1.0, -1.0);
+    //CGContextScaleCTM(context, 1.0, -1.0);
+    
+    CGFloat dpi = 0.5;
     
     CGPDFDocumentRef pdfRef= [PdfManager getPdf:_fileNo];
     if (pdfRef!=NULL)
@@ -88,23 +90,41 @@
         
         if (pdfPage != NULL) {
             
-            CGContextSaveGState(context);
+            CGRect pageRect = CGPDFPageGetBoxRect(pdfPage, kCGPDFMediaBox);
+            
+            CGSize new_size;
+            new_size.height = pageRect.size.width * dpi;
+            new_size.width = pageRect.size.height * dpi;
+            
+            UIGraphicsImageRenderer *renderer = [[UIGraphicsImageRenderer alloc] initWithSize:new_size];
+            
+            CGFloat scale_x = self.bounds.size.width / new_size.width;
+            CGFloat scale_y = self.bounds.size.height / new_size.height;
+            
+            CGFloat scale = scale_x < scale_y ? scale_x : scale_y;
+            
             CGRect pageBounds;
-                pageBounds = CGRectMake(0,
-                                        -self.bounds.size.height,
-                                        self.bounds.size.width,
-                                        self.bounds.size.height);
+            pageBounds = CGRectMake((self.bounds.size.width - new_size.width * scale) / 2,
+                                    (self.bounds.size.height - new_size.height * scale) / 2,
+                                    new_size.width * scale,
+                                    new_size.height * scale);
+            
+            UIImage *image = [renderer imageWithActions:^(UIGraphicsImageRendererContext * _Nonnull context) {
+                [[UIColor whiteColor] set];
+                [context fillRect:CGRectMake(0, 0, new_size.width, new_size.height)];
+                CGContextTranslateCTM(context.CGContext, 0.0, new_size.height);
+                CGContextScaleCTM(context.CGContext, dpi, dpi);
+                CGContextRotateCTM(context.CGContext, -M_PI/2);
+                CGContextDrawPDFPage(context.CGContext, pdfPage);
+            }];
+            
+
             
             // Fill the background with white.
             CGContextSetRGBFillColor(context, 1.0,1.0,1.0,1.0);
-            CGContextFillRect(context, pageBounds);
+            CGContextFillRect(context, self.bounds);
             
-            CGAffineTransform pageTransform = CGPDFPageGetDrawingTransform(pdfPage, kCGPDFCropBox, pageBounds, 0, true);
-            CGContextConcatCTM(context, pageTransform);
-            
-            CGContextDrawPDFPage(context, pdfPage);
-            CGContextRestoreGState(context);
-            
+            CGContextDrawImage(context, pageBounds, image.CGImage);
             RLog(@"drawpage %d", _page);
         }
 
